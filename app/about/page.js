27cdf94e49558ea1fd2a5b25d2d0d76142a4a608
Bianca Velázquez <biancavlz@users.noreply.github.com>
2026-04-21 13:08:00 +0200

    Add link to main page

--- app/about/page.js
+++ app/about/page.js
@@ -1,5 +1,12 @@
+import Link from "next/link";
+
 function Page() {
-  return <h1>About</h1>;
+  return (
+    <div>
+      <h1>About</h1>
+      <Link href="/cabins">Explore luxury cabins</Link>
+    </div>
+  );
 }
 
 export default Page;
