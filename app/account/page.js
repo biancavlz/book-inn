0265dc270da0b2c68ea56bea033f50ca254065d3
Bianca Velázquez <biancavlz@users.noreply.github.com>
2026-04-22 18:01:53 +0200

    improve pages

--- app/account/page.js
+++ app/account/page.js
@@ -1,7 +1,11 @@
 export const metadata = { title: "Account" };
 
 function Page() {
-  return <h1>Account</h1>;
+  return (
+    <h2 className="font-semibold text-2xl text-accent-400 mb-7">
+      Welcome, user name
+    </h2>
+  );
 }
 
 export default Page;
