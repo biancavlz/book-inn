1c13aa976468a103d56a2f6f15851862346b4da0
Bianca Velázquez <biancavlz@users.noreply.github.com>
2026-04-27 11:14:33 +0200

    Add FilterButton component and highlight active button

--- app/_components/Filter.js
+++ app/_components/Filter.js
@@ -1,11 +1,13 @@
 "use client";
 
 import { usePathname, useRouter, useSearchParams } from "next/navigation";
+import FilterButton from "./FilterButton";
 
 function Filter() {
   const searchParms = useSearchParams();
   const router = useRouter();
   const pathname = usePathname();
+  const activeFilter = searchParms.get("capacity") ?? "all";
 
   function handleFilter(filter) {
     const params = new URLSearchParams(searchParms);
@@ -16,30 +18,37 @@ function Filter() {
 
   return (
     <div className="border border-primary-800 flex">
-      <button
-        className="px-5 py-2 hover:bg-primary-700"
-        onClick={() => handleFilter("all")}
+      <FilterButton
+        filter="all"
+        handleFilter={handleFilter}
+        activeFilter={activeFilter}
       >
         All cabins
-      </button>
-      <button
-        className="px-5 py-2 hover:bg-primary-700"
-        onClick={() => handleFilter("small")}
+      </FilterButton>
+
+      <FilterButton
+        filter="small"
+        handleFilter={handleFilter}
+        activeFilter={activeFilter}
       >
         1&mdash;3 guess
-      </button>
-      <button
-        className="px-5 py-2 hover:bg-primary-700"
-        onClick={() => handleFilter("medium")}
+      </FilterButton>
+
+      <FilterButton
+        filter="medium"
+        handleFilter={handleFilter}
+        activeFilter={activeFilter}
       >
         4&mdash;7 guess
-      </button>
-      <button
-        className="px-5 py-2 hover:bg-primary-700"
-        onClick={() => handleFilter("large")}
+      </FilterButton>
+
+      <FilterButton
+        filter="large"
+        handleFilter={handleFilter}
+        activeFilter={activeFilter}
       >
         8&mdash;12 guess
-      </button>
+      </FilterButton>
     </div>
   );
 }
